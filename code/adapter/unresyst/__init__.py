f34980be4dc50b7b63e3bb8885bcbaeb72b650b7
pcvpcvpcv <pcvpcvpcv@16512c99-5954-7620-b85c-3e22edfc4e26>
2010-10-31 19:05:55 +0000

    Created a project structured as adapter to a third party system

---
+++ code/adapter/unresyst/__init__.py
@@ -0,0 +1,7 @@
+"""A package holding the whole unresyst application.
+
+The only class to be used outside is the Recommender.
+"""
+
+from recommender.base import Recommender
+from recommender.rules import *
---
+++ code/adapter/unresyst/recommender/__init__.py
@@ -0,0 +1,4 @@
+"""Package holding the Recommender class, holding the interface for the whole
+unresyst."""
+
+from base import Recommender
