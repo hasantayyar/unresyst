d13a8f4e832ad45484ee793d4e76258877872ddf
pcvpcvpcv <pcvpcvpcv@16512c99-5954-7620-b85c-3e22edfc4e26>
2010-11-14 20:56:05 +0000

    finished examples in the architecture section, basic structure of models

---
+++ code/adapter/unresyst/constants.py
@@ -0,0 +1,7 @@
+"""The constants used in Unresyst"""
+
+MAX_LENGTH_NAME = 40
+"""The maximum length of the name in the universal representation."""
+
+MAX_LENGTH_CLASS_NAME = 20
+"""The maximum length of the class name."""
---
+++ code/adapter/unresyst/models/abstractor.py
@@ -0,0 +1,6 @@
+"""Models which instances are created by the abstractor package.
+
+A representation of each defined role and relationship.
+"""
+
+from django.db import models
