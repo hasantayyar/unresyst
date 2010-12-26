2e5f710e32c46da0bbbf468bc3a85b6e2f8286f5
pcvpcvpcv <pcvpcvpcv@16512c99-5954-7620-b85c-3e22edfc4e26>
2010-12-26 21:32:34 +0000

    aggregator - added tests, fixed bugs

--- code/adapter/unresyst/models/abstractor.py
+++ code/adapter/unresyst/models/abstractor.py
@@ -62,13 +62,23 @@ class RelationshipInstance(BaseRelationshipInstance, ContentTypeModel):
         unique_together = ('subject_object1', 'subject_object2', 'definition')
         """For each definition there can be only one subject-object pair."""
 
-    def get_linear_expectancy(self):
+    def get_linear_expectancy(self, _redirect_to_leaf=True):
         """Gets the linearly aggregated parameters for instance expectancy.
         In this class returns the weight.
+        
+        @type redirect_to_leaf: bool
+        @param redirect_to_leaf: if True, the call is redirected to the leaf class.
+            When called from outside _redirect_to_leaf should always be True, unless
+            it's sure it's called on the leaf class instance.
         
         @rtype: float from [0, 1]
         @return: the aggregated expectancy.
         """
+        
+        # redirect to leaf or not
+        if _redirect_to_leaf:
+            return self.as_leaf_class().get_linear_expectancy(_redirect_to_leaf=False)
+        
         return self.definition.as_leaf_class().weight
 
 
@@ -80,14 +90,20 @@ class RuleInstance(RelationshipInstance):
     A number from [0, 1].
     """
     
-    def get_linear_expectancy(self):
+    def get_linear_expectancy(self, _redirect_to_leaf=True):
         """Gets the linearly aggregated parameters for instance expectancy.
-        In this class returns the weight.
+        Multiplies the base class weight by expectancy.
+
+        @type redirect_to_leaf: bool
+        @param redirect_to_leaf: if True, the call is redirected to the leaf class.
+            When called from outside _redirect_to_leaf should always be True, unless
+            it's sure it's called on the leaf class instance.        
         
         @rtype: float from [0, 1]
         @return: the aggregated expectancy.
         """
-        base_expectancy = super(RuleInstance, self).get_linear_expectancy()
+        base_expectancy = super(RuleInstance, self).get_linear_expectancy(
+                                                    _redirect_to_leaf=False)
         
         return base_expectancy * self.expectancy
         
--- code/adapter/unresyst/tests/test_base.py
+++ code/adapter/unresyst/tests/test_base.py
@@ -2,6 +2,11 @@
 
 from django.test import TestCase
 
+from unresyst.models.common import SubjectObject 
+
+from demo.recommender import ShoeRecommender
+from demo.models import User, ShoePair
+
 class DBTestCase(TestCase):
     """A base class for all tests which need database testing data"""
 
@@ -11,3 +16,63 @@ class DBTestCase(TestCase):
         # insert test data
         from demo.save_data import save_data
         save_data()
+
+class TestBuild(DBTestCase):
+    """The base class performing build in the setup."""
+
+    def setUp(self):
+        """The setup for all tests - build the recommender"""
+        super(TestBuild, self).setUp()
+
+        # call the tested function        
+        ShoeRecommender.build()
+
+
+class TestEntities(TestBuild):
+    """The base class adding universal and specific entitits 
+    to the test instance
+    """
+    
+    def setUp(self):
+        """Obtain specific and universal subject objects 
+        and store them in the test instance
+        """ 
+        
+        super(TestEntities, self).setUp()
+        
+        self.specific_entities = {
+            'Alice': User.objects.get(name="Alice"),
+            'Bob': User.objects.get(name="Bob"),
+            'Cindy': User.objects.get(name="Cindy"),
+            'Sneakers': ShoePair.objects.get(name="Sneakers"),
+            "Rubber Shoes": ShoePair.objects.get(name="Rubber Shoes"),
+            'RS 130': ShoePair.objects.get(name='RS 130'),
+        }
+        
+        rm = ShoeRecommender.recommender_model
+        self.universal_entities = {
+            'Alice': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['Alice'], 
+                            entity_type='S', 
+                            recommender=rm),
+            'Bob': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['Bob'], 
+                            entity_type='S', 
+                            recommender=rm),
+            'Cindy': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['Cindy'], 
+                            entity_type='S', 
+                            recommender=rm),                            
+            'Sneakers': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['Sneakers'], 
+                            entity_type='O', 
+                            recommender=rm),
+            'Rubber Shoes': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['Rubber Shoes'], 
+                            entity_type='O', 
+                            recommender=rm),
+            'RS 130': SubjectObject.get_domain_neutral_entity(
+                            domain_specific_entity=self.specific_entities['RS 130'], 
+                            entity_type='O', 
+                            recommender=rm),
+        }         
